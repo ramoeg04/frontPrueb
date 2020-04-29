e217106c37f2bfbde2773cb805a6f7e1b48a9664
Geomar Salas <4494.geo@gmail.com>
2020-04-29 13:02:41 -0400

    Prueba 3

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://hardcore-jang-75f419.netlify.app/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
